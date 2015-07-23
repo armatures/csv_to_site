30cb703f0da6f7a4fe046cb490e7f9f2eb155d0e
Charlie Bevis <charliebevis@users.noreply.github.com>
2015-07-22 22:04:51 -0700

    removing unused files

--- erb_example.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby
-require 'erb'
-require 'ostruct'
-namespace = OpenStruct.new(title: 'Mom Attacks', director: 'George')
-template = "Title: <%= title %> \n Director: <%= director %>"
-puts ERB.new(template).result(namespace.instance_eval { binding })
